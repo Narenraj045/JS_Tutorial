6dcb0c2c834bdeee130219437b312cde6458dde4
Narenraj045 <narenraj045@gmail.com>
2025-09-24 10:16:04 +0000

    conversion operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score1=33;
+let score2="35";
+let score3="35acb";
+
+
+console.log(typeof score1);
+console.log(typeof(score1)); // as a method
+
+console.log(typeof score2);
+console.log(typeof(score2)); // as a method
+
+// here the score2 value comes as string , but suppose we know 100% that we are going to do operation in numbers then 
+
+let valueInnumber = Number(score2);
+console.log(typeof valueInnumber);
+
+let vauleInNumber2 = Number(score3);
+console.log(vauleInNumber2); // coming as not a number 
+console.log(typeof vauleInNumber2);
+
+let score4 = null;
+console.log(typeof score4);
+
+let score5 = undefined;
+console.log(typeof score5);
+
+// when we are converting a number to number then it is easily converted ("33" => 33)
+// "33abc" => NaN its not a number but when we check typeof then it shows number
+//true converts as 1, and false as 0, 
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+
+// boolean conversion 
+// 1 => true;  0 => false; 
+// "" => false; 
+//"Naren" => true
+
+
+let somenumber=33;
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+ 
