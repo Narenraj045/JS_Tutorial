a70ac01d0d3697926db22dd526095c7f80b5fa2d
Narenraj045 <narenraj045@gmail.com>
2025-09-30 08:25:49 +0000

    number and maths and string

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -2,12 +2,54 @@ const name = " Naren "
 const repoCount = 50;
  console.log(name + repoCount + " value"); // this syntax to concatinate is not good , try to ignore 
 
- // we will use backtick in place of the that where we have string interpolation 
+ // we will use backtick in place of that where we have string interpolation 
  // actually we create placeholders and we can inject variable directly 
 
 
  console.log(`Hello, Many name is ${name} and my repo count is ${repoCount}`);
  
 // one more way to declare string 
-const gameName = new String("Narengame")
+const gameName = new String("Narengame");
+
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__); // different functions 
+console.log(gameName.length); // different functions 
+console.log(gameName.toUpperCase()); // different functions 
+console.log(gameName.charAt(2)); // different functions 
+console.log(gameName.indexOf('g')); // different functions 
+
+
+const newString = gameName.substring(0,5);
+console.log(newString);
+
+const anotherString = gameName.slice(2,5);
+console.log(anotherString);
+
+const anotherString2 = gameName.slice(-8,3); // we can even use minus value in slice function , it will count from reverse
+console.log(anotherString2);
+
+
+const newStringOne = "    Naren Kumar  ";
+console.log(newStringOne);
+console.log(newStringOne.trim()); // trim function removes the blank spaces 
+
+
+const url = "https://naren.com/naren%20kumar"
+console.log(url.replace('%20', '_'));
+
+console.log(url.includes('naren')); // we can even ask question 
+console.log(url.includes('nikhil')); // we can even ask question
+
+console.log(url.split('.'));
+
+
+
+
+
+
+
+
+
+
  
---
+++ 01_Basics/06_num_and_math.js
@@ -0,0 +1,55 @@
+const score = 400;
+
+// console.log(score);
+
+const balance = new Number(500);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber= 123.898347;
+
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(2));
+
+
+const hundreds = 100000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(5.6));
+// console.log(Math.ceil(5.2));
+
+// console.log(Math.floor(5.2));
+// console.log(Math.floor(5.8));
+
+// console.log(Math.min(2, 4, 8, 1, 5));
+
+console.log(Math.random()); // it always gives a value between 0 and 1 
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
+
+
+
+
+
+
